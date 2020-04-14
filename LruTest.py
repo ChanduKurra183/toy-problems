78358c84ee0c308e0b0db46507294a79bcf0d659
Chandu_Kurra <chandukurra143@msitprogram.net>
2020-04-14 12:39:08 +0530

    updated

---
+++ LruTest.py
@@ -0,0 +1,22 @@
+import LRU
+
+class LruTest:
+    def testcases(self):
+
+        a = lru(2)
+        a.put("social_media")
+        assert a.get("social_media") == "1"
+        print("Testcase passed..!")
+
+        a.put("browser")
+        assert a.get("browser") == "2"
+        print("Testcase passed..!!")
+        print("<< All testcases passed >>")
+
+        lis = a.get_cache()
+        for i in lis:
+            print(i)
+
+if _name_ == '_main_':
+    a = LruTest()
+    a.testcases()
